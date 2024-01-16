6eabe85e91f1c8b0fc81d065fd92954201cd7210
Azim Khan(Nil) <119257178+Azimkhan404@users.noreply.github.com>
2024-01-16 16:24:12 +0600

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os,platform
 os.system('git pull')
  
 jkw=platform.architecture()[0]
-if sun=="32bit":
+if run=="32bit":
     print('Sorry 32 Bit Not Supported...')
 elif sun=="64bit":
      __import__("sun")
