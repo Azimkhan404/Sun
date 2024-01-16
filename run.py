edf17b01e2b2871040dfa37ba122fc9c85dd8f74
Azim Khan(Nil) <119257178+Azimkhan404@users.noreply.github.com>
2024-01-16 16:20:58 +0600

    Add files via upload

---
+++ run.py
@@ -0,0 +1,8 @@
+import os,platform
+os.system('git pull')
+ 
+jkw=platform.architecture()[0]
+if sun=="32bit":
+    print('Sorry 32 Bit Not Supported...')
+elif sun=="64bit":
+     __import__("sun")
